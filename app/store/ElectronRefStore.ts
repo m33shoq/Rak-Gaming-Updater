595e009ac970eeed33621b3369d13ad2f6d45d56
m33shoq <mishadyrov@gmail.com>
2025-06-12 19:23:38 +0300

    fix prettier; remove old html

--- app/store/ElectronRefStore.ts
+++ app/store/ElectronRefStore.ts
@@ -7,24 +7,26 @@ const storedRefs: Record<string, any> = {};
 interface ElectronStoreRef<T> extends Ref<T> {}
 
 export function getElectronStoreRef<T>(key: string, defaultValue: T): ElectronStoreRef<T> {
-  if (key in storedRefs) {
-    return storedRefs[key] as Ref<T>;
-  }
+	if (key in storedRefs) {
+		return storedRefs[key] as Ref<T>;
+	}
 
-  const refValue = shallowRef<T>(defaultValue);
-  storedRefs[key] = refValue;
-  api.store.get(key).then((value: T) => {
-    refValue.value = value;
-    watch(refValue, (newValue) => {
-    //   log.info(`Updating store value for key "${key}":`, newValue);
-      api.store.set(key, newValue).catch((error: any) => {
-        log.error(`Failed to set store value for key "${key}":`, error);
-      });
-    });
-    api.store.onSync(key, (syncValue: T) => {
-      log.info(`Store value for key "${key}" synced:`, syncValue);
-      refValue.value = syncValue;
-    });
-  });
-  return refValue;
+	const refValue = shallowRef<T>(defaultValue);
+	storedRefs[key] = refValue;
+	api.store.get(key).then((value: T) => {
+		refValue.value = value;
+		watch(refValue, (newValue) => {
+			//   log.info(`Updating store value for key "${key}":`, newValue);
+			api.store.set(key, newValue).catch((error: any) => {
+				log.error(`Failed to set store value for key "${key}":`, error);
+			});
+		});
+		api.store.onSync(key, (syncValue: T) => {
+			if (refValue.value !== syncValue) {
+				log.info(`Store value for key "${key}" synced:`, syncValue);
+				refValue.value = syncValue;
+			}
+		});
+	});
+	return refValue;
 }
