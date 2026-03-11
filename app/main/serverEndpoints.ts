8bcfe469036b82f0c2b29da23e817f6e1f2c03ad
m33shoq <mishadyrov@gmail.com>
2026-03-11 02:08:51 +0200

    move to the new server

--- app/main/serverEndpoints.ts
+++ app/main/serverEndpoints.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 console.log('Using server URL:', process.env.ELECTRON_USE_DEV_URL);
-export const SERVER_URL = process.env.ELECTRON_USE_DEV_URL === '1' ? 'http://localhost:3001' : `https://rak-gaming-annoucer-bot-93b48b086bae.herokuapp.com`;
+export const SERVER_URL = process.env.ELECTRON_USE_DEV_URL === '1' ? 'http://localhost:3001' : `https://rak-gaming-updater.org`//`https://rak-gaming-annoucer-bot-93b48b086bae.herokuapp.com`;
 export const SERVER_LOGIN_ENDPOINT = `${SERVER_URL}/login`;
 export const SERVER_UPLOADS_ENDPOINT = `${SERVER_URL}/upload`;
 export const SERVER_EXISTING_FILES_ENDPOINT = `${SERVER_URL}/files`;
