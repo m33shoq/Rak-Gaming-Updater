03a58244c1e62945ddde27c3e2512f4aea8e8345
m33shoq <mishadyrov@gmail.com>
2026-05-09 09:31:27 +0300

    fix bad merge

--- app/renderer/store/ReviewsStore.ts
+++ app/renderer/store/ReviewsStore.ts
@@ -177,7 +177,6 @@ export const useReviewsStore = defineStore('Reviews', () => {
 	}
 
 	watch(selectedReportCode, async (newVal, oldVal) => {
-	watch(selectedReportCode, async (newVal, oldVal) => {
 		if (newVal !== oldVal) {
 			selectedFightID.value = null; // reset selected fight
 			savedFightEvents.value = {}; // clear cached fight events
